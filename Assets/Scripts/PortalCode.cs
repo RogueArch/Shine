c3b5342355d36f5089e65a11b97f35b1bf5b50cb
RogueArch <caiomendes2704@gmail.com>
2022-06-10 22:29:18 -0300

    Arrumar Bugs e ajeitar a primeira fase

--- Assets/Scripts/PortalCode.cs
+++ Assets/Scripts/PortalCode.cs
@@ -19,7 +19,7 @@ public class PortalCode : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
