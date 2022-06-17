7e9a2aefdd7ab618078d75691f7b26039d09fc1d
RogueArch <caiomendes2704@gmail.com>
2022-06-17 14:47:41 -0300

    Deathcounter na tela

--- Assets/Scripts/Morte.cs
+++ Assets/Scripts/Morte.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Morte : MonoBehaviour
 {
 
     public GameObject implosion;
+    public float faleceu;
+    public Text mycounter;
     public int life = 1;
     public GameObject SpawnPoint;
     public int DeathCount = 0;
+
+
     private void OnParticleCollision(GameObject other)
     {
         life--;
@@ -17,12 +22,18 @@ public class Morte : MonoBehaviour
         {
             Instantiate(implosion, transform.position, Quaternion.identity);
             //Destroy(gameObject);//
-            if(SpawnPoint){
-                transform.position=SpawnPoint.transform.position;
+            if (SpawnPoint)
+            {
+                transform.position = SpawnPoint.transform.position;
                 DeathCount = DeathCount + 1;
-            }else{
+                faleceu = DeathCount;
+                mycounter.text = faleceu.ToString("0");
+            }
+            else
+            {
                 Destroy(gameObject);
             }
         }
+
     }
 }
